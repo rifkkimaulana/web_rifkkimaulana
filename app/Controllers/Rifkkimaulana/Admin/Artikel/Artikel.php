4bd4ddd6e6459169b56537980be50587750f2578
rifkkimaulana <rifkkimaulana@gmail.com>
2023-10-14 11:02:05 +0700

    new update

--- app/Controllers/Rifkkimaulana/Admin/Artikel/Artikel.php
+++ app/Controllers/Rifkkimaulana/Admin/Artikel/Artikel.php
@@ -54,52 +54,50 @@ class Artikel extends BaseController
 
     public function new_post()
     {
-        if ($this->request->getMethod() === 'post') {
 
-            $artikelModel = new ArtikelModel();
+        $artikelModel = new ArtikelModel();
 
-            // Get and handle kategori_id
-            $kategoriIds = $this->request->getPost('kategori_id');
-            $kategoriIdString = is_array($kategoriIds) ? implode(',', $kategoriIds) : '0';
+        // Get and handle kategori_id
+        $kategoriIds = $this->request->getPost('kategori_id');
+        $kategoriIdString = is_array($kategoriIds) ? implode(',', $kategoriIds) : '0';
 
-            // Get and handle tag_id
-            $tagIds = $this->request->getPost('tag_id');
-            $tagIdString = is_array($tagIds) ? implode(',', $tagIds) : '0';
+        // Get and handle tag_id
+        $tagIds = $this->request->getPost('tag_id');
+        $tagIdString = is_array($tagIds) ? implode(',', $tagIds) : '0';
 
-            $judul = $this->request->getPost('judul');
+        $judul = $this->request->getPost('judul');
 
-            // Menghapus karakter khusus dan mengganti spasi dengan tanda hubung
-            $slug = preg_replace('/[^A-Za-z0-9-]+/', '-', $judul);
+        // Menghapus karakter khusus dan mengganti spasi dengan tanda hubung
+        $slug = preg_replace('/[^A-Za-z0-9-]+/', '-', $judul);
 
-            // Mengubah huruf menjadi huruf kecil
-            $slug = strtolower($slug);
+        // Mengubah huruf menjadi huruf kecil
+        $slug = strtolower($slug);
 
-            // Menghapus strip atau tanda hubung berlebihan
-            $slug = trim($slug, '-');
+        // Menghapus strip atau tanda hubung berlebihan
+        $slug = trim($slug, '-');
 
 
-            $data = [
-                'judul' => $judul,
-                'isi' => $this->request->getPost('isi'),
-                'kategori_id' => $kategoriIdString,
-                'tag_id' => $tagIdString,
-                'status' => $this->request->getPost('status'),
-                'slug' => $slug
-            ];
+        $data = [
+            'judul' => $judul,
+            'isi' => $this->request->getPost('isi'),
+            'kategori_id' => $kategoriIdString,
+            'tag_id' => $tagIdString,
+            'status' => $this->request->getPost('status'),
+            'slug' => $slug
+        ];
 
-            $gambar = $this->request->getFile('gambar');
+        $gambar = $this->request->getFile('gambar');
 
-            if ($gambar->isValid() && !$gambar->hasMoved()) {
-                $namaUnik = $gambar->getRandomName();
-                $gambar->move(FCPATH . 'assets/image/artikel/', $namaUnik);
+        if ($gambar->isValid() && !$gambar->hasMoved()) {
+            $namaUnik = $gambar->getRandomName();
+            $gambar->move(FCPATH . 'assets/image/artikel/', $namaUnik);
 
-                $data['cover'] = $namaUnik;
-            }
-
-            $artikelModel->insertData($data);
-
-            return redirect()->to(base_url('artikel'))->with('success', 'Artikel berhasil ditambahkan.');
+            $data['cover'] = $namaUnik;
         }
+
+        $artikelModel->insertData($data);
+
+        return redirect()->to(base_url('artikel'))->with('success', 'Artikel berhasil ditambahkan.');
     }
 
     public function ubah($id)
@@ -121,49 +119,47 @@ class Artikel extends BaseController
 
     public function ubah_post()
     {
-        if ($this->request->getMethod() === 'post') {
 
-            $artikelModel = new ArtikelModel();
+        $artikelModel = new ArtikelModel();
 
-            $id = $this->request->getPost('id');
+        $id = $this->request->getPost('id');
 
-            // Get and handle kategori_id
-            $kategoriIds = $this->request->getPost('kategori_id');
-            $kategoriIdString = is_array($kategoriIds) ? implode(',', $kategoriIds) : '0';
+        // Get and handle kategori_id
+        $kategoriIds = $this->request->getPost('kategori_id');
+        $kategoriIdString = is_array($kategoriIds) ? implode(',', $kategoriIds) : '0';
 
-            // Get and handle tag_id
-            $tagIds = $this->request->getPost('tag_id');
-            $tagIdString = is_array($tagIds) ? implode(',', $tagIds) : '0';
+        // Get and handle tag_id
+        $tagIds = $this->request->getPost('tag_id');
+        $tagIdString = is_array($tagIds) ? implode(',', $tagIds) : '0';
 
-            $data = [
-                'judul' => $this->request->getPost('judul'),
-                'isi' => $this->request->getPost('isi'),
-                'kategori_id' => $kategoriIdString,
-                'tag_id' => $tagIdString,
-                'status' => $this->request->getPost('status'),
-            ];
+        $data = [
+            'judul' => $this->request->getPost('judul'),
+            'isi' => $this->request->getPost('isi'),
+            'kategori_id' => $kategoriIdString,
+            'tag_id' => $tagIdString,
+            'status' => $this->request->getPost('status'),
+        ];
 
-            $gambar = $this->request->getFile('gambar');
-            $gambarLama = $this->request->getPost('gambar_lama');
+        $gambar = $this->request->getFile('gambar');
+        $gambarLama = $this->request->getPost('gambar_lama');
 
-            if ($gambar->isValid() && !$gambar->hasMoved()) {
-                $namaUnik = $gambar->getRandomName();
-                $gambar->move(FCPATH . 'assets/image/artikel/', $namaUnik);
+        if ($gambar->isValid() && !$gambar->hasMoved()) {
+            $namaUnik = $gambar->getRandomName();
+            $gambar->move(FCPATH . 'assets/image/artikel/', $namaUnik);
 
-                if (!empty($gambarLama)) {
-                    $gambarPath = FCPATH . 'assets/image/artikel/' . $gambarLama;
-                    if (file_exists($gambarPath)) {
-                        unlink($gambarPath);
-                    }
+            if (!empty($gambarLama)) {
+                $gambarPath = FCPATH . 'assets/image/artikel/' . $gambarLama;
+                if (file_exists($gambarPath)) {
+                    unlink($gambarPath);
                 }
-
-                $data['cover'] = $namaUnik;
             }
 
-            $artikelModel->updateId($id, $data);
-
-            return redirect()->to(base_url('artikel'))->with('success', 'Artikel berhasil diubah.');
+            $data['cover'] = $namaUnik;
         }
+
+        $artikelModel->updateId($id, $data);
+
+        return redirect()->to(base_url('artikel'))->with('success', 'Artikel berhasil diubah.');
     }
 
     public function delete($id)
